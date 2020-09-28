da846b65256bd58b30a2baab006a853b7dbefe79
yzhang33 <youmingzhang19@gmail.com>
2020-09-28 16:37:11 -0400

    Updates

---
+++ precache-manifest.5ca305b8fcca5542bdd8ecdac8fb7be7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5810942dab4404794f0882464144a0d",
+    "url": "/data_api/index.html"
+  },
+  {
+    "revision": "f807e001bf87753c1c13",
+    "url": "/data_api/static/css/main.ffa29014.chunk.css"
+  },
+  {
+    "revision": "6c945d36e800dd361c50",
+    "url": "/data_api/static/js/2.c4878b8c.chunk.js"
+  },
+  {
+    "revision": "801455085fc6f57b23eb009849543052",
+    "url": "/data_api/static/js/2.c4878b8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f807e001bf87753c1c13",
+    "url": "/data_api/static/js/main.47297e3c.chunk.js"
+  },
+  {
+    "revision": "92db016add479bf37b73",
+    "url": "/data_api/static/js/runtime-main.f0d2021c.js"
+  }
+]);
